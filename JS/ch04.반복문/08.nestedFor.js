176f028195e7db0ae8999acac18a72791f13b973
Younjung58 <skykh999@naver.com>
2024-07-17 17:01:52 +0900

    javascript

--- JS/ch04.반복문/08.nestedFor.js
+++ JS/ch04.반복문/08.nestedFor.js
@@ -45,3 +45,30 @@ for(let i=0;i<6;i++){
     diceArray.push(dice);
 }
 console.log(diceArray);
+
+// 6. 구구단 만들기
+for(let i=2; i<=9; i++){
+    for(let k=1; k<=9; k++){
+        console.log(i,'x',k,'=',i*k);
+    }
+    console.log();
+}
+
+// 다이아몬드 그리기
+let N = 4;      // 가운데 두꺼운 별이 0개
+for (let i=0; i<=N; i++){
+    let line =[];
+    for (let k=0; k<N-i; k++)
+        line += ' ';
+    for (let k=0; k <2*i+1; k++)
+        line += '*';
+    console.log(line);
+}
+for (let i=N-1; i>=0; i--){
+    let line =[];
+    for (let k=0; k<N-i; k++)
+        line += ' ';
+    for (let k=0; k <2*i+1; k++)
+        line += '*';
+    console.log(line);
+}
---
+++ JS/ch04.반복문/a.연습문제.js
@@ -0,0 +1,7 @@
+// 정수 자료형의 이름
+let N = 25;
+let product='';
+for(let i=1; i<=(N/4); i++){
+    product+='long '
+}
+console.log(product+'int');
