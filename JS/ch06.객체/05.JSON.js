b54e3916cfa6c5e144d0b07956c8b4d1faa5038f
Younjung58 <skykh999@naver.com>
2024-07-19 14:12:36 +0900

    JS 객체

--- JS/ch06.객체/05.JSON.js
+++ JS/ch06.객체/05.JSON.js
@@ -15,3 +15,22 @@ console.log(internalForm);
 
 let personArray = [person, {name: 'Maria', age: 25, job: '디자이너'}];
 console.log(JSON.stringify(personArray));  // [{"name":"James","age":27,"job":"프로그래머"},{"name":"Maria","age":25,"job":"디자이너"}]
+console.log(personArray);
+
+// p = 
+// [
+//   { id: 1, name: 'James', age: 27, job: '프로그래머' },
+//   { id: 2, name: 'Maria', age: 25, job: '디자이너' }
+// ]
+// ->  이름, 나이, 직업 중에 어떤걸로 정렬할거야?
+
+// comparator
+// (어떠한 객체를 정렬할 기준을 내가 정해서 알려주는거)
+
+// (a, b) => a - b		오름차순
+// (a, b) => b - a		내림차순
+
+// 컴퓨터는 '크다, 작다, 같다'만 비교함
+
+// p.sort((a, b) = > a.id - b.id);		id의 오름차순으로 정렬해라
+// p.sort((a, b) = > b.age - a.age);	age의 내림차순으로 정렬해라
